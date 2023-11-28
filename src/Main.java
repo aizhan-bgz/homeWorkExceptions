50713948b1c9678b397e357bcbceb740466a1acf
aizhan-bgz <baigazakova1999@gmail.com>
2023-11-28 21:59:18 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число: ");
+        try {
+            Integer number = scanner.nextInt();
+            if (number < 0) throw new NegativeNumberException("Ошибка! Введено отрицательное число");
+        } catch (NegativeNumberException exception){
+            System.err.println(exception.getMessage());
+            exception.printStackTrace();
+        } catch (InputMismatchException inputMismatchException) {
+            System.err.println("Ошибка! Введено недопустимое значение");
+            inputMismatchException.printStackTrace();
+        }
+    }
+}
